0526741b53f3f17cda9bcc1830bcb9d16d3e604f
email <jordanmorel.ca@gmail.com>
2025-03-15 22:03:11 -0500

    mantener la sesion abierta

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleCart } from "../redux/shopSlice";
 import { Link, useNavigate } from "react-router-dom"; // Importamos useNavigate
@@ -9,6 +9,23 @@ const Navbar = () => {
   const dispatch = useDispatch();
   const isCartOpen = useSelector((state) => state.shop.isCartOpen);
   const navigate = useNavigate(); // Hook para la navegación
+
+  const [loggedUser, setLoggedUser] = useState(null);
+
+  // Verificar si hay usuario en localStorage al cargar la página
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setLoggedUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  // Función para cerrar sesión
+  const handleLogout = () => {
+    localStorage.removeItem("user"); // Eliminar usuario
+    setLoggedUser(null); // Actualizar estado
+    navigate("/"); // Redirigir a inicio
+  };
 
   console.log("Estado en Navbar:", isCartOpen);
 
@@ -27,10 +44,17 @@ const Navbar = () => {
           <button className="cart__button" onClick={() => dispatch(toggleCart())}>
             <img src={carrito} alt="shopping cart" />
           </button>
-          {/* Botón de login ahora navega a /login */}
-          <button className="cart__button" onClick={() => navigate("/Login")}>
-            <img src={account_login} alt="login Account" />
-          </button>
+
+          {/* Si el usuario está logueado, mostrar "Cerrar sesión" */}
+          {loggedUser ? (
+            <button className="cart__button" onClick={handleLogout}>
+              <img src={account_login} alt="Cerrar sesión" />
+            </button>
+          ) : (
+            <button className="cart__button" onClick={() => navigate("/Login")}>
+              <img src={account_login} alt="login Account" />
+            </button>
+          )}
         </div>
         {isCartOpen && (
           <div className="cart">
