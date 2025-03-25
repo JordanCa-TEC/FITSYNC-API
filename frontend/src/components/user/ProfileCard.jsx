753636b4bf440718c18837a9e96f7c84532807f3
email <jordanmorel.ca@gmail.com>
2025-03-24 20:57:31 -0500

    Nuevo avance para la informacion de usuario

--- frontend/src/components/user/ProfileCard.jsx
+++ frontend/src/components/user/ProfileCard.jsx
@@ -1,16 +1,24 @@
 // ProfileCard.jsx
 import React from "react";
 import "../../sass/_userDashboard.scss";
+import { usuariofoto } from "../../assets/assets";
+
 
 const ProfileCard = () => {
   return (
-    <div className="profile-card">
-      <div className="profile-avatar"></div>
-      <div className="profile-info">
-        <h2>Nombre de Usuario</h2>
-        <p>Objetivo: Quemar grasa / Tonificar</p>
-      </div>
+
+<div className="profile-card">
+  <div className="profile-background">
+    <div className="profile-avatar">
+      <img src={usuariofoto} alt="usuario-fitzync" />
     </div>
+  </div>
+  <div className="profile-info">
+    <h2>Nombre de Usuario</h2>
+    <p><strong>Objetivo:</strong> Quemar grasa / Tonificar</p>
+  </div>
+</div>
+
   );
 };
 
